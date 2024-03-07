ff81f09f8602ff8e5ac78f8f964cc96f3707fcf7
Alberto Paim <albertoopaim@gmail.com>
2024-03-07 10:12:23 -0300

    to deploy

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv'
 import router from './routes/admin';
 import routerClient from './routes/client';
+import cors from "cors"
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(routerClient);
 app.use(router);
 
