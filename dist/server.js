327ec644bba6b7d919fcee9d806870ac8a532513
Alberto Paim <albertoopaim@gmail.com>
2024-03-07 10:49:05 -0300

    build

---
+++ dist/server.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const admin_1 = __importDefault(require("./routes/admin"));
+const client_1 = __importDefault(require("./routes/client"));
+const cors_1 = __importDefault(require("cors"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.use(client_1.default);
+app.use(admin_1.default);
+app.listen(process.env.PORT || 3000);
